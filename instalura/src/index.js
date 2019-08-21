8e9f65952b00f35def199ee976a72777b57ec4ea
Ollyver Ottoboni <ollyver.ottoboni@amcom.com.br>
2019-08-21 13:41:39 -0300

    Aula 04_01 - 1- Liberando o acesso para timeline pública

--- instalura/src/index.js
+++ instalura/src/index.js
@@ -3,28 +3,65 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {BrowserRouter} from "react-router-dom";
-import {Switch, Route} from 'react-router-dom'
-import {Redirect} from 'react-router-dom'
+import {Switch, Route, matchPath } from 'react-router-dom'
+import {Redirect} from 'react-router-dom';
+
+// corrigir o erro nesta aula
+import createBrowserHistory from 'history/createBrowserHistory';
+
 
 // Meus Componentes
 import Login from "./componentes/Login";
 import Logout from "./componentes/Logout";
 
-function validaAutenticacao(){
-    console.log('Valida Autenticacao da Index.js ');
+const validaAutenticacao = () => {
+    const history = createBrowserHistory();
+    const match = matchPath(history.location.pathname,  {path: '/timeline/:login'});
+
+    const privateRoute = match === null;
+
+    console.log('Valida Autenticacao da Index.js');
+
+    console.log('----------------------------------------');
+    console.log('Constante history', history);
+
+    console.log('----------------------------------------');
+    console.log('Constante match', match);
+
+    console.log('----------------------------------------');
+    console.log('Constante privateRoute', privateRoute);
+
+    return (privateRoute && localStorage.getItem('auth-token')) === null;
+};
+
+/*
+// Substitui essa parte pela parte acima para corrigir o erro nesta aula
+function validaAutenticacao(args){
+    console.log('Valida Autenticacao da Index.js ', args);
 
     if(localStorage.getItem('auth-token') != null)
     {
         return true;
     }
-}
+}*/
 
 ReactDOM.render(
     <BrowserRouter>
         <Switch>
             <Route path="/" exact={true} component={Login} />
             <Route path="/logout" component={Logout} />
-            <Route path="/timeline" render={() => (
+
+            <Route
+                path="/timeline"
+                render={() => (
+                    validaAutenticacao() ?
+                        (<Redirect to="/?msg=Você precisa estar logado." />) : (<App />)
+                )}
+            />
+
+{/*
+// Substitui essa parte pela parte acima para corrigir o erro nesta aula
+<Route path="/timeline/:login?" render={() => (
                 validaAutenticacao() ? (
                     <App />
                 ) : (
@@ -36,7 +73,7 @@ ReactDOM.render(
                     />
 
                 )
-            )} />
+            )} />*/}
 
             {/*Definindo Rota Padrão, caso as demais não sejam chamadas*/}
             <Route component={Login}/>
